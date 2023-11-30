4a943a8f357a5017b63103bd26b243fbddc29c1b
sbpark88 <saebyulpark88@gmail.com>
2023-11-30 14:41:51 +0900

    Add: 'watch' example

--- react-hook-form-register/src/pages/WatchComponent.tsx
+++ react-hook-form-register/src/pages/WatchComponent.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+
+type FormInputs = {
+  name: string;
+  password: string;
+};
 
 const WatchComponent = () => {
-  return <>Watch</>;
+  const { register, watch } = useForm<FormInputs>();
+
+  const watchName = watch("name");
+
+  return (
+    <form>
+      <input type="text" placeholder="User Name" {...register("name")} />
+      <input type="password" placeholder="Password" {...register("password")} />
+      <p>Name: {watchName}</p>
+    </form>
+  );
 };
 
 export default WatchComponent;

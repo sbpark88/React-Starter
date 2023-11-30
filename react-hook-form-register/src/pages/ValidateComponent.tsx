a8f8fbee7f61c2a97136ecb2af50a8df6974d27b
sbpark88 <saebyulpark88@gmail.com>
2023-11-30 19:00:08 +0900

    Add: 'Validate' example

--- react-hook-form-register/src/pages/ValidateComponent.tsx
+++ react-hook-form-register/src/pages/ValidateComponent.tsx
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+
+type FormInputs = {
+  name: string;
+  age: number;
+  address: string;
+  password: string;
+};
 
 const ValidateComponent = () => {
-  return <>Validate</>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormInputs>();
+  const [result, setResult] = useState<string>("");
+  const [success, setSuccess] = useState<boolean>(false);
+
+  const onFormSubmit = useCallback((form: FormInputs) => {
+    setResult(JSON.stringify(form));
+  }, []);
+
+  useEffect(() => {
+    errors.name || errors.age ? setSuccess(false) : setSuccess(true);
+  }, [errors, errors.name, errors.age]);
+
+  return (
+    <form onSubmit={handleSubmit(onFormSubmit)}>
+      <input
+        type="text"
+        {...register("name", { required: true, minLength: 5 })}
+        placeholder="Name"
+      />
+      <input
+        type="number"
+        {...register("age", { required: true, min: 10, max: 30 })}
+        placeholder="Age"
+      />
+      <input type="text" {...register("address")} placeholder="Address" />
+      <input type="text" {...register("password")} placeholder="Password" />
+      <input type="submit" />
+      <p>{result}</p>
+      <p>Validate: {String(success)}</p>
+    </form>
+  );
 };
 
 export default ValidateComponent;

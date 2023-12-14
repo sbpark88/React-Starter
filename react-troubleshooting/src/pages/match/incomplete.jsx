59f6c0aa6073180046728cb10cdc0308a10fb626
sbpark88 <saebyulpark88@gmail.com>
2023-12-15 00:17:04 +0900

    Fix: Asynchronous function state falsification

--- react-troubleshooting/src/pages/match/incomplete.jsx
+++ react-troubleshooting/src/pages/match/incomplete.jsx
@@ -11,33 +11,31 @@ class MatchPage extends Component {
       currentCard: createMockCard(),
       matches: [],
     };
-
-    this.next = this.next.bind(this)
-    this.like = this.like.bind(this)
-  }
-
-  next() {
-    this.setState({
-      ...this.state,
-      currentCard: createMockCard(),
-    });
-  }
-
-  like() {
-    this.next();
-
-    checkIsMatched().then(({ data: { isMatched } }) => {
-      if (isMatched) {
-        this.setState({
-          ...this.state,
-          matches: [this.state.currentCard, ...this.state.matches],
-        });
-      }
-    });
   }
 
   render() {
-    const { state: { currentCard, matches }, next, like } = this;
+    const { state: { currentCard, matches } } = this;
+
+    const next = () => {
+      this.setState({
+        ...this.state,
+        currentCard: createMockCard(),
+      });
+    }
+
+    const like = () => {
+      next();
+
+      checkIsMatched().then(({ data: { isMatched } }) => {
+        if (isMatched) {
+          this.setState({
+            ...this.state,
+            matches: [currentCard, ...this.state.matches],
+          });
+        }
+      });
+    }
+
     const matchControllerProps = { next, like };
 
     return (
